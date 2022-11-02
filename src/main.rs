aa057d6ba42fca7a7465ce9b2fbb730ed43d9833
pjpjg <pjgreaves96@googlemail.com>
2022-11-02 13:55:37 +0000

    Implements NQuery struct & begins implementation.

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,59 @@ use graphbench::editgraph::EditGraph;
 use graphbench::graph::*;
 use graphbench::io::*;
 
-use graphbench::ordgraph::*;
+use graphbench::degengraph::*;
 use itertools::*;
 
 use fxhash::{FxHashMap, FxHashSet};
 
 fn main() {
 
-    let mut graph = EditGraph::from_gzipped("Yeast.txt.gz").expect("File not found");
+    let mut graph = EditGraph::from_gzipped("Yeast.txt.gz").expect("File not found");   
+
     graph.remove_loops();
 
-    let graph = OrdGraph::by_degeneracy(&graph);
+    let nquery = NQuery::new(&graph);
 
-    let mut R:FxHashMap<Vec<Vertex>, u32> = FxHashMap::default();
+}
 
-    for u in graph.vertices() {
-        let N = graph.left_neighbours(u);
+struct NQuery{
+    R:FxHashMap<Vec<Vertex>, u32>,
+    graph:DegenGraph
+}
 
-        for subset in N.into_iter().powerset() {
-            R.entry(subset).and_modify(|c| *c += 1).or_insert(1);
+impl NQuery {
+    fn new(graph: &EditGraph) -> Self{
+
+        let graph = DegenGraph::from_graph(graph);    
+
+        let mut R:FxHashMap<Vec<Vertex>, u32> = FxHashMap::default();
+
+        for u in graph.vertices() {
+            let N = graph.left_neighbours(u);
+
+            for subset in N.into_iter().powerset() {
+
+                R.entry(subset).and_modify(|c| *c += 1).or_insert(1);
+
+            }
         }
+        NQuery { R, graph }
     }
 
-    for (X, count) in R.iter() {
-        println!("{X:?} -> {count}");
+    fn is_shattered(&self, S: Vec<Vertex>) -> bool{
+        unimplemented!()
+    }
+
+    fn query(&self, X: FxHashSet<Vertex>, S: FxHashSet<Vertex>) -> u32{
+
+        let S_minus_X:FxHashSet<_> = S.difference(&X).collect();
+
+        for subset in S_minus_X.into_iter().powerset() {
+
+            let subset: FxHashSet<_> = subset.iter().cloned().collect();
+
+            let Y = X.union(&subset);
+
+        }
     }
 }
