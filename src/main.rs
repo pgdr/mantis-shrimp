afd919a5b3381e240900d518171503276272a031
pjpjg <pjgreaves96@googlemail.com>
2022-11-08 16:53:36 +0000

    Imps left_neighbourhood_set() and begins imp of is_shattered().

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(non_snake_case)]
+use std::backtrace::Backtrace;
 use std::collections::BTreeSet;
+use std::default;
 
 use graphbench::editgraph::EditGraph;
 use graphbench::graph::*;
@@ -17,35 +19,43 @@ fn main() {
 
 }
 
-struct NQuery{
+struct NQuery {
     R:FxHashMap<BTreeSet<Vertex>, u32>,
     graph:DegenGraph
 }
 
 impl NQuery {
-    fn new(graph: &EditGraph) -> Self{
+    fn new(graph: &EditGraph) -> Self {
         let graph = DegenGraph::from_graph(graph);    
         let mut R:FxHashMap<_, _> = FxHashMap::default();
 
         for u in graph.vertices() {
             let N = graph.left_neighbours(u);
+
             for subset in N.into_iter().powerset() {
                 R.entry(subset.into_iter().collect()).and_modify(|c| *c += 1).or_insert(1);
-
             }
         }
         NQuery { R, graph }
     }
 
-    fn is_shattered(&self, S: Vec<Vertex>) -> bool{
-        unimplemented!()
-        // loop over all subsets of S
-        // run query_uncor for each subset
-        // store the result in a HashMap 
-        // (then we will apply the correction)
+    fn is_shattered(&self, S: &BTreeSet<Vertex>) -> bool {
+        let mut I:FxHashMap<_, _> = FxHashMap::default();
+
+        for subset in S.iter().powerset(){ 
+            let subset: BTreeSet<_> = subset.into_iter().cloned().collect();
+            let res = self.query_uncor(&subset, S);
+            I.insert(subset, res);
+        }
+
+        let left_neighs = self.left_neighbour_set(S);
+
+        for neigh in left_neighs {
+            // apply correction
+        }
     }
 
-    fn query_uncor(&self, X: BTreeSet<Vertex>, S: BTreeSet<Vertex>) -> i32{
+    fn query_uncor(&self, X: &BTreeSet<Vertex>, S: &BTreeSet<Vertex>) -> i32 {
         let S_minus_X:BTreeSet<_> = S.difference(&X).collect();
         let mut res:i32 = 0;
 
@@ -53,13 +63,23 @@ impl NQuery {
             let subset: BTreeSet<u32> = subset.into_iter().cloned().collect();
             let Y:BTreeSet<u32> = X.union(&subset).cloned().collect();
 
-            if subset.len() % 2 == 0{
+            if subset.len() % 2 == 0 {
                 res += *self.R.get(&Y).unwrap_or(&0) as i32;
-            } else{
+            } else {
                 res -= *self.R.get(&Y).unwrap_or(&0) as i32;
             }
         }
         // to do: add correction.
         res
-    } 
+    }
+
+    fn left_neighbour_set(&self, S: &BTreeSet<Vertex>) -> BTreeSet<Vertex> {
+        let mut res: BTreeSet<Vertex> = BTreeSet::default();
+
+        for u in S {
+            let l_neigh = self.graph.left_neighbours(u);
+            res.extend(l_neigh.into_iter())
+        }
+        res  
+    }
 }
