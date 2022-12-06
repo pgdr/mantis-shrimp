7aa11b5dcc55c0fbc4725a9866a0d07203dc16d3
Felix <felix.reidl@gmail.com>
2022-12-06 16:13:19 +0000

    Adds command line parsing, first part of algorithm

---
+++ src/io.rs
@@ -0,0 +1,27 @@
+use std::path::Path;
+use std::ffi::OsStr;
+
+use graphbench::editgraph::EditGraph;
+
+pub fn load_graph(file:&Path) -> Result<EditGraph, String> {
+    if !(file.exists() && file.is_file()) {
+        return Err(format!("The provided file `{file:?}` does not exist or is a directory."));
+    }
+
+    let extension = file.extension().and_then(OsStr::to_str);
+
+    match extension {
+        Some("txt") => {
+            Ok(EditGraph::from_txt(file.to_str().unwrap()).unwrap())
+        }
+        Some("gz") => {
+            Ok(EditGraph::from_gzipped(file.to_str().unwrap()).unwrap())
+        }
+        Some(_) => {
+            Err(format!("Invalid file `{file:?}`. The supported formats are `.txt.gz` and `.txt`."))
+        }        
+        None => {
+            Err(format!("Invalid file `{file:?}`. The supported formats are `.txt.gz` and `.txt`."))
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![allow(non_snake_case)]
+
+mod io;
+
 // use std::backtrace::Backtrace;
 use std::collections::BTreeSet;
+use io::load_graph;
 
 use graphbench::editgraph::EditGraph;
 use graphbench::graph::*;
@@ -10,11 +14,69 @@ use itertools::*;
 
 use fxhash::FxHashMap;
 
-fn main() {
-    let mut graph = EditGraph::from_gzipped("Yeast.txt.gz").expect("File not found");   
-    graph.remove_loops();
-    // let nquery = NQuery::new(&graph);
+use clap::{Parser, ValueEnum};
+use std::path::Path;
 
+/// Counts 'butterflies' (4-cycles) in sparse bipartite networks.
+#[derive(Parser, Debug)]
+#[clap(author, version="1.0", about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    help: bool,
+
+    /// The network file
+    file:String,    
+}
+
+
+
+fn main() -> Result<(), &'static str> {
+    let args = Args::parse();
+    let filename = args.file.clone();
+
+    // Load graph
+    let path = Path::new(&filename);
+    let mut graph = match load_graph(path) {
+        Ok(G) => G,
+        Err(msg) => {
+            println!("{msg}");
+            return Err("Parsing error");
+        }
+    };
+
+    println!("Loaded graph with n={} and m={}", graph.num_vertices(), graph.num_edges());
+
+    // let mut graph = EditGraph::from_gzipped("Yeast.txt.gz").expect("File not found");   
+    graph.remove_loops();
+    let nquery = NQuery::new(&graph);
+
+    let d = *nquery.graph.left_degrees().values().max().unwrap();
+    let p = ((d as f64).log2() + 1.0).floor() as u32;
+    
+    /*
+        Idea: Use bloom filter to store the k-sized shattered sets, then
+        query to figure out whether a given (k+1)-sized set could be shattered
+    */
+
+    // Case 2 in paper: Shattered set is small
+    let vertices:Vec<_> = graph.vertices().collect();
+    for r in 3..=p { 
+        println!("Searching sets of size r={r}");
+        for S in vertices.iter().combinations(r as usize) {
+            let S:BTreeSet<Vertex> = S.into_iter().cloned().cloned().collect();
+            if nquery.is_shattered(&S) {
+                println!("  Found shattered set for r={r}: {S:?}");
+                break;
+            }
+        } 
+    }
+
+    // Case 1 in paper: Shattered set is large
+    for r in (p+1)..(d+1) {
+        // TODO
+    }
+
+    Ok(())
 }
 
 struct NQuery {
